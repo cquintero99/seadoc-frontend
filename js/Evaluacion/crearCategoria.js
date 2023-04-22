6aca42d4921150737a107a88ca28b6dc1c727779
cquintero99 <ciroalfonsoqude@ufps.edu.co>
2023-04-22 14:55:40 -0500

    agrego funciones para eliminar y actulizar preguntas de la evaluacion

--- js/Evaluacion/crearCategoria.js
+++ js/Evaluacion/crearCategoria.js
@@ -33,7 +33,7 @@ crearCategoria.addEventListener('click', () => {
 
         //Verifico que el arreglo de categorias no esta vacio
         if (primer != null) {
-          array=  array.concat(JSON.parse(primer))
+            array = array.concat(JSON.parse(primer))
         }
 
         //Obtengo el nombre
@@ -62,7 +62,7 @@ crearCategoria.addEventListener('click', () => {
 
         document.getElementById("bodyListaCategoria").
             innerHTML += `<h4 id="list-item-${posicion}"  class="text-center"> # ${posicion} - ${nombre}   </h4>
-                <div class="input-group">
+                <div class="input-group mb-3">
                 
                 <input type="text" class="form-control" id="crearPreguntaCategoria${posicion}" placeholder="Crear pregunta para la categoria ${nombre}" aria-label="Input group example" aria-describedby="btnGroupAddon">
                 <button id="btnAddPregunta" onclick="crearPregunta(${posicion})" class="input-group-text btn btn-success" type="button"><i class="fa fa-plus"
@@ -70,12 +70,13 @@ crearCategoria.addEventListener('click', () => {
             </div>
             </br>
             
-            <ol class="list-group list-group-numbered list-group-flush" id="preguntaC${posicion}">
+            <ol class="list-group list-group-numbered list-group-flush" >
                 
                 </ol>
+            <div  class="list-group list-group-numbered list-group-flush" id="preguntaC${posicion}"><div>
                 
             
-            <hr>`
+            `
 
 
         setTimeout(() => {
@@ -84,7 +85,12 @@ crearCategoria.addEventListener('click', () => {
         }, 100)
 
     } else {
-        //Nombre vacio
+        document.getElementById("validCategoria").innerHTML = `<div class="alert alert-warning" role="alert">
+        Ingresa el nombre de la categoria
+      </div>`
+        setTimeout(() => {
+            document.getElementById("validCategoria").innerHTML = ""
+        }, 5000)
         ocultarSpinner()
     }
 
@@ -99,48 +105,51 @@ function crearPregunta(pos) {
     //Busco las preguntas guardadas
     const preguntasEvaluacion = sessionStorage.getItem("preguntasEvaluacion")
     //Busco numero de pregunta
-    let nPregunta=sessionStorage.getItem("nPregunta")
-    
+    let nPregunta = sessionStorage.getItem("nPregunta")
+
     //Verifico el numero de preguntas
-    if(nPregunta===null){
-        nPregunta=0;
-    }else{
-        
+    if (nPregunta === null) {
+        nPregunta = 0;
+    } else {
+
     }
     if (pregunta != "") {
-       //Creo la posicion que es nPregunta+1
-       let posicion=Number(nPregunta)+1
-       //Creo el array Pregunta
-       let arrayPregunta=[]
+        //Creo la posicion que es nPregunta+1
+        let posicion = Number(nPregunta) + 1
+        //Creo el array Pregunta
+        let arrayPregunta = []
         //Creo el objeto pregunto
-        
+
         const newPregunta = {
-            id:posicion,
-            idCategoria:pos,
+            id: posicion,
+            idCategoria: pos,
             pregunta
         }
         //Si la el array no esta vacio
-        if(preguntasEvaluacion!=null){
-         arrayPregunta=  arrayPregunta.concat(JSON.parse(preguntasEvaluacion))
+        if (preguntasEvaluacion != null) {
+            arrayPregunta = arrayPregunta.concat(JSON.parse(preguntasEvaluacion))
         }
         //Guardo la pregunta nueva
         arrayPregunta.push(newPregunta)
-        sessionStorage.setItem("preguntasEvaluacion",JSON.stringify(arrayPregunta))
-        sessionStorage.setItem("nPregunta",posicion)
+        sessionStorage.setItem("preguntasEvaluacion", JSON.stringify(arrayPregunta))
+        sessionStorage.setItem("nPregunta", posicion)
 
         //Agrego la vista
         document.getElementById("preguntaC" + pos).innerHTML += `
-            <li class="list-group-item" id="${pos}-${posicion}" >
-            <div class="input-group">
-
-                    <input type="text" id="inputCategoriaP" class="form-control" value="${pregunta}"
+            <div id="${pos}-${posicion}">
+            <div class="input-group " >
+                    <input type="text" id="input${pos}-${posicion}" class="form-control" value="${pregunta}"
                     placeholder="CREAR CATEGORIA EVALUACION" aria-label="Input group example"
-                    aria-describedby="btnGroupAddon">
+                    aria-describedby="btnGroupAddon" disabled readonly>
+                    
+                    <button  onclick="actualizarPregunta('${pos}-${posicion}')" class="input-group-text btn btn-warning" type="button">
+                    <i class="fa fa-pencil" aria-hidden="true"></i></button>
                     <button id="crearCategoria" onclick="eliminarPregunta('${pos}-${posicion}')" class="input-group-text btn btn-danger" type="button">
                     <i class="fa fa-times" aria-hidden="true"></i></button>
-                </div>
-            
-            </li>`
+                    </div>
+                    <hr>
+           </div> 
+                 `
         document.getElementById("crearPreguntaCategoria" + pos).value = ""
     }
 }
@@ -151,21 +160,80 @@ window.addEventListener('load', () => {
     document.getElementById("nombreUsuario").innerHTML = `<h5 class="mt-3 p-3 mb-3  text-light " >Bienvenido _ ${nombre} </h5>`
 });
 */
-function eliminarPregunta(id){
-    document.getElementById(id).remove()
-    let con=id.split("-")
-    let pregunta=con[1]
-    const array=[]
-    const ListaP = sessionStorage.getItem('preguntasEvaluacion')
-    ListaP.slice(pregunta,1)
-    array.push(ListaP)
-    sessionStorage.setItem('preguntasEvaluacion',JSON.stringify(array ))
-
-
-   
+function actualizarPregunta(id) {
+    console.log(id)
+    let pregunta = document.getElementById("input" + id).value
+    console.log(pregunta)
+    document.getElementById(id).innerHTML = `<div class="input-group " >
+    <input type="text" id="input${id}" class="form-control" value="${pregunta}"
+    placeholder="CREAR CATEGORIA EVALUACION" aria-label="Input group example"
+    aria-describedby="btnGroupAddon" >
+    
+    <button  onclick="cambiarPregunta('${id}')" class="input-group-text btn btn-success" type="button">
+    <i class="fa fa-check" aria-hidden="true"></i></button>
+    <button id="crearCategoria" onclick="eliminarPregunta('${id}')" class="input-group-text btn btn-danger" type="button">
+    <i class="fa fa-times" aria-hidden="true"></i></button>
+    </div>
+    <hr>`
 }
 
+function cambiarPregunta(id) {
+    let con = id.split("-")
+    //obtengo el id de la pregunta
+    let idPregunta = con[1]
+    //Obtengo la nueva pregunta
+    let pregunta = document.getElementById("input" + id).value
+    console.log(idPregunta)
+    //Busco la lista de preguntas
+    const preguntas = JSON.parse(sessionStorage.getItem("preguntasEvaluacion"));
+    //Busco la pregunta que voy actualizar
+    const newPregunta = preguntas.find(p => p.id === Number(idPregunta));
+    //Actualizo la pregunta
+    newPregunta.pregunta = pregunta;
+    //Guardo la lista de preguntas
+    sessionStorage.setItem("preguntasEvaluacion", JSON.stringify(preguntas));
+    //Muestro la vista
+    document.getElementById(id).innerHTML = `<div class="input-group " >
+    <input type="text" id="input${id}" class="form-control" value="${pregunta}"
+    placeholder="CREAR CATEGORIA EVALUACION" aria-label="Input group example"
+    aria-describedby="btnGroupAddon" disabled readonly>
+    
+    <button  onclick="actualizarPregunta('${id}')" class="input-group-text btn btn-warning" type="button">
+    <i class="fa fa-pencil" aria-hidden="true"></i></button>
+    <button id="crearCategoria" onclick="eliminarPregunta('${id}')" class="input-group-text btn btn-danger" type="button">
+    <i class="fa fa-times" aria-hidden="true"></i></button>
+    </div>
+    <hr>`
 
+}
+function eliminarPregunta(id) {
+    //Quito la pregunta de la interfas
+    document.getElementById(id).remove()
+    let con = id.split("-")
+    //obtengo el id de la pregunta
+    let pregunta = con[1]
+    //Creo el array nuevo
+    let array = []
+    //obtengo la lista de pregutnas
+    const ListaP = sessionStorage.getItem('preguntasEvaluacion')
+
+    //agrego las preguntas al array
+    array = array.concat(JSON.parse(ListaP))
+
+    //Busco la pregunta que voy a eliminar
+    const index = array.findIndex(element => element.id === Number(pregunta));
+    if (index !== -1) {
+        //elimino la pregunta del array
+        array.splice(index, 1);
+    }
+    //guardo el array en el sessionStorage
+    sessionStorage.setItem('preguntasEvaluacion', JSON.stringify(array))
+
+
+
+}
+
+//Boton salir
 const salir = document.getElementById("salir")
 
 
