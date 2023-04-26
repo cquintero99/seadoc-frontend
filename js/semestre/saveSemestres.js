8e259f245f416a800c4bf759c7f0b3130aaef520
cquintero99 <ciroalfonsoqude@ufps.edu.co>
2023-04-25 21:07:49 -0500

    agrego los metodos para guardar semestre

---
+++ js/semestre/saveSemestres.js
@@ -0,0 +1,68 @@
+const agregarSemestre=document.getElementById("agregarSemestre")
+
+agregarSemestre.addEventListener('click',()=>{
+    mostrarSpinner()
+    let fechaInicio=document.getElementById("fechaInicio").value
+    let fechaFin=document.getElementById("fechaFin").value
+    let usuarioId=JSON.parse(localStorage.getItem("data")).id
+
+    const semestre={
+        fechaInicio,
+        fechaFin,
+        fechaRegistro:"",
+        nombre:"",
+        estado:"ESPERA",
+        visibilidad:"PRIVADO",
+        usuarioId
+        
+    }
+    registrarSemestre(semestre)
+    .then(response=>response.json())
+    .then(data=>{
+        console.log(data)
+        body = `<div class="alert alert-success" role="alert">
+        Semestre se guardo con exito
+      </div>`;
+document.getElementById("alertSemestre").innerHTML = body;
+
+setTimeout(() => {
+  document.getElementById("alertSemestre").innerHTML = "";
+}, 5000);
+        verSemestres()
+    })
+    .catch(err=>{
+        console.log(err)
+        body = `<div class="alert alert-danger" role="alert">
+        Error Solo se pueden registrar 2 semestres en un año
+      </div>`;
+document.getElementById("alertSemestre").innerHTML = body;
+ocultarSpinner()
+setTimeout(() => {
+  document.getElementById("alertSemestre").innerHTML = "";
+}, 7000);
+    })
+    .finally(final=>{
+        
+        ocultarSpinner()
+        
+
+    })
+
+
+
+})
+
+async function registrarSemestre(semestre){
+    console.log(semestre)
+    let token=localStorage.getItem("token")
+    const result= await fetch(urlBasic+"/semestre/save",{
+        method:'POST',
+        body:JSON.stringify(semestre),
+        headers:{
+            "Authorization":"Bearer "+token,
+            "Content-type":"Application/json"
+        }
+    })
+    return result
+    
+}
--- js/semestre/semestre.js
+++ js/semestre/semestre.js
@@ -1,52 +1,47 @@
 //CONEXION CON EL BACK
 const urlBasic = "https://teacher-test-backend-production-e58a.up.railway.app";
+//"http://localhost:8080"
+
 const tabla = document.getElementById("tabla-datos");
 const cuerpoTabla = tabla.getElementsByTagName("tbody")[0];
-function mostrarSpinner() {
-  document.getElementById("sppiner").innerHTML=`<div id="spinner-container" class="d-flex justify-content-center align-items-center ">
-    <div class="spinner-border text-danger" role="status">
-      <span class="sr-only">Cargando...</span>
-    </div>
-  </div>`
-}
 
-function ocultarSpinner() {
-  document.getElementById("sppiner").innerHTML=`<div id="spinner-container" class="d-flex justify-content-center align-items-center d-none">
-    <div class="spinner-border text-danger" role="status">
-      <span class="sr-only">Cargando...</span>
-    </div>
-  </div>`
-}
-async function verSemestres() {
-  mostrarSpinner()
+
+verSemestres()
+async function listaSemestres(){
   let token = localStorage.getItem("token");
 
-  await fetch(urlBasic + "/semestre", {
+  const result= await fetch(urlBasic + "/semestre", {
     method: "GET",
     headers: {
       Authorization: "Bearer " + token,
       "Content-type": "Application/json",
     },
   })
+  return result
+
+}
+
+
+async function verSemestreEstado(estado){
+ 
+  let token=localStorage.getItem("token")
+  const result= await fetch(urlBasic+"/semestre/estado/"+estado,{
+      method:'GET',
+      
+      headers:{
+          "Authorization":"Bearer "+token,
+          "Content-type":"Application/json"
+      }
+  })
+  return result
+  
+}
+async function verSemestres() {
+  mostrarSpinner()
+    listaSemestres()
     .then((response) => response.json())
     .then((data) => {
-      let body = ``;
-
-      for (let i = 0; i < data.length; i++) {
-        body += `
-          <tr>
-          <td>${data[i].id}</td>
-          <td>${new Date(data[i].fechaInicio).toLocaleDateString()}</td>
-          <td>${new Date(data[i].fechaFin).toLocaleDateString()}</td>
-          <td>${data[i].usuarioId}</td>
-          <td>${new Date(data[i].fechaRegistro).toLocaleDateString()}</td>
-          <td>${data[i].estado}</td>
-          </tr>
-
-          `;
-        console.log(JSON.stringify(data));
-      }
-      document.getElementById("tabla_semestres").innerHTML = body;
+      mostrarData(data)
     })
     .finally((yes) => {
       //loadingElement.style.display = 'none';
@@ -57,72 +52,74 @@ async function verSemestres() {
     });
 }
 
-// Obtener los elementos del DOM
-verSemestres();
-const botonAgregar = document.getElementById("agregar-semestre");
-botonAgregar.addEventListener("click", agregarFila);
 
-const modal = document.getElementById("modal");
-const botonCerrar = document.getElementById("cerrar");
-botonCerrar.addEventListener("click", cerrarModal);
+function mostrarData(data){
+  
+  let body = ``;
+  let actual=false;
+  for (let i = data.length-1; i >=0 ; i--) {
+    if(data[i].estado=="ACTUAL"){
+      document.getElementById("semestreActual").innerHTML=data[i].nombre+" --"+data[i].visibilidad
+      body += `
+      <tr>
+      <td>${i+1}</td>
+      <td>${data[i].nombre}</td>
+      <td>${new Date(data[i].fechaInicio).toLocaleDateString()}</td>
+      <td>${new Date(data[i].fechaFin).toLocaleDateString()}</td>
+      <td>${data[i].estado}</td>
+      <td>${new Date(data[i].fechaRegistro).toLocaleDateString()}</td>
+      <td>${data[i].visibilidad}</td>
+      <td>
+      <div class="btn-group dropstart">
+        <button type="button" class="btn btn-warning dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+          Actualizar
+        </button>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Estado</a></li>
+          <li><a class="dropdown-item" href="#">Visibilidad</a></li>
+          <li><a class="dropdown-item" href="#">Fechas</a></li>
+        </ul>
+      </div>
+      </td>
+      </tr>
 
-function agregarFila() {
-  modal.style.display = "block";
-}
+      `
+    }else{
+    body += `
+      <tr>
+      <td>${i+1}</td>
+      <td>${data[i].nombre}</td>
+      <td>${new Date(data[i].fechaInicio).toLocaleDateString()}</td>
+      <td>${new Date(data[i].fechaFin).toLocaleDateString()}</td>
+      <td>${data[i].estado}</td>
+      <td>${new Date(data[i].fechaRegistro).toLocaleDateString()}</td>
+      <td>${data[i].visibilidad}</td>
+      <td>
+      
+      </td>
+      </tr>
 
-function cerrarModal() {
-  modal.style.display = "none";
-}
-
-/*Boton */
-
-const botonGuardar = document.getElementById("agregar");
-botonGuardar.addEventListener("click", agregarSemestre);
-botonGuardar.addEventListener("click", cerrarModal);
-
-
-
-//NOTA: Completar función porra conectar con el back <------
-async function agregarSemestre(evento) {
-  evento.preventDefault();
-  const formulario = document.getElementById("datos-semestre");
-  const datos = new FormData(formulario);
-
-  console.log(datos);
-
-  //Aqui no sé si deba ir el id del semestre
-  const fechaInicio = new Date(datos.get("fechaInicio")).toISOString();
-  const fechaFin = new Date(datos.get("fechaFin")).toISOString();
-  const fechaRegistro = new Date().toISOString();
-  const estado = "activo";
-  const usuarioID = 1; // Cambiar a un número autogenerado, de momento se deja así
-
-  const semestre = {
-    fechaInicio,
-    fechaFin,
-    fechaRegistro,
-    estado,
-    usuarioID,
-  };
-  console.log(semestre);
-
-  try {
-    const response = await fetch(urlBasic + "/semestre", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(semestre),
-    });
-
-    if (response.ok) {
-      alert("Semestre agregado");
-    } else {
-      alert("Semestre no agregado");
-
-      throw new Error("Error al guardar los datos del semestre");
+      `
+     
     }
-  } catch (error) {
-    console.log(error);
   }
+ 
+  document.getElementById("tabla_semestres").innerHTML = body;
+}
+
+function mostrarSpinner() {
+  document.getElementById("sppiner").innerHTML=`<div id="spinner-container" class="d-flex justify-content-center align-items-center ">
+    <div class="spinner-border text-danger" role="status">
+      <span class="sr-only">Cargando...</span>
+    </div>
+  </div>`
+}
+
+
+function ocultarSpinner() {
+  document.getElementById("sppiner").innerHTML=`<div id="spinner-container" class="d-flex justify-content-center align-items-center d-none">
+    <div class="spinner-border text-danger" role="status">
+      <span class="sr-only">Cargando...</span>
+    </div>
+  </div>`
 }
