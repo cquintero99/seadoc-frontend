53a53508dacd81f8debfdfdba95d1dd22c48b04d
cquintero99 <ciroalfonsoqude@ufps.edu.co>
2023-05-04 11:59:19 -0500

    agrego metodo para eliminar semestre

--- js/Evaluacion/listadoEvaluacions.js
+++ js/Evaluacion/listadoEvaluacions.js
@@ -218,4 +218,31 @@ function ocultarSpinner() {
       </div>
     </div>`
 }
-
+$(document).ready( function () {
+    $('#tablaEvaluaciones').DataTable({
+        "language": {
+            "decimal":        "",
+            "emptyTable":     "No hay datos disponibles en la tabla",
+            "info":           "Mostrando _START_ a _END_ de _TOTAL_ registros",
+            "infoEmpty":      "Mostrando 0 a 0 de 0 registros",
+            "infoFiltered":   "(filtrado de _MAX_ registros totales)",
+            "infoPostFix":    "",
+            "thousands":      ",",
+            "lengthMenu":     "Mostrar _MENU_ registros",
+            "loadingRecords": "Cargando...",
+            "processing":     "Procesando...",
+            "search":         "Buscar:",
+            "zeroRecords":    "No se encontraron registros coincidentes",
+            "paginate": {
+                "first":      "Primero",
+                "last":       "Último",
+                "next":       "Siguiente",
+                "previous":   "Anterior"
+            },
+            "aria": {
+                "sortAscending":  ": activar para ordenar de manera ascendente",
+                "sortDescending": ": activar para ordenar de manera descendente"
+            }
+        }
+    });
+  })
